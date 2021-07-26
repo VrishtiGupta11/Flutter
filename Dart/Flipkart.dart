9c158459a46e6fc4bf294c28b9a870978a1df247
Vrishti Gupta <53931644+VrishtiGupta11@users.noreply.github.com>
2021-07-26 13:57:42 +0530

    Create Flipkart.dart

---
+++ Dart/Flipkart.dart
@@ -0,0 +1,47 @@
+// Flipkart Attributes
+
+/*
+  ListOfProducts / Category
+    Electronics, Hosiery, Grocery, Toys, TravellingKit, Fashion, Furniture, etc.
+  Product
+    Name, Price, ProductCode, Category
+  TypesOfUsers
+    Customers, Sellers, Developers, SalesMan, CustomerCarePersons
+  User 
+    EmailAddress, Name, ID, Address, PhoneNumber, TypeOfUsers
+  Order
+    OrderNo., date, time, Products, total, Customer, Sellers
+  Customer
+    Order, User
+  Sellers
+    Order, User
+  ShoppingCart
+    Product, Discount, PromoCode, Order, User
+  PaymentMOde 
+    Card, Netbanking, CashOnDelivery, Bank
+*/
+
+// Class
+
+class User {
+  String? email;
+  String? name;
+  int? ID;
+  String? address;
+  int? number;
+
+  User({this.email, this.name, this.ID, this.address, this.number}) {
+    print("User Details: ");
+    print('${email}\n${name}\n${ID}\n${address}\n${number}\n');
+  }
+}
+
+
+void main() {
+  User user1 = new User(
+      email: 'ivrishtigupta@gmail.com',
+      name: 'Vrishti Gupta',
+      ID: 1915086,
+      address: '1234 Ludhiana',
+      number: 1234984784);
+}
